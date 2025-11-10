0069015cf37c9c8ae28ce668d22daf4a358780bf
Yvan-David <irankundayvan2020@gmail.com>
2025-11-10 14:31:58 +0200

    created next app

---
+++ v1-portfolio/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#0f172a] font-sans dark:bg-[#0f172a] ">
+      <main className="flex min-h-screen w-full max-w-6xl flex-row items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className=" w-full max-w-screen min-h-screen bg-white dark:bg-white">
+<p>erdtgyuh</p>
+        </div>
+        <div className=" w-full min-h-screen bg-red-500 dark:bg-red-500">
+    <p>dfghjk</p>
+        </div>
+      </main>
+    </div>
+  );
+}
